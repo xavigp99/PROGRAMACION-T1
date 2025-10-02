18ebf32ab15282cdecc0b01a8f58363b50836ca2
xavigp99 <xaviergp99@gmail.com>
2025-10-02 19:50:09 +0200

    update

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,2 +1,22 @@
 public class Main {
+
+    // esto es un comentario
+    /*
+    Este comentario
+    se puede dividir
+    en varias lineas de codigo
+     */
+    //TODO me falta esto
+
+    public static void main(String[] args){
+        System.out.println("Este es mi primer programa en Java");
+        System.out.print("Esta es la segunda linea del programa\n");
+    }
+    
+    public void nombreMetodo (){
+
+    }
+
+    //TODO Esto tambien me falta
+
 }
